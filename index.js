e7dab595c17a9109a9cb82e5f06842731cec392f
arif87 <arif.ibrahm@gmail.com>
2019-09-10 20:52:36 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bp      = require('body-parser');
+const db      = require('./src/db');
+
+app           = express();
+
+app.use(bp.json());
+
+const _port   = process.env.PORT || 4000;
+
+app.listen(_port, ()=>{
+    console.log(`Server running on port ${_port}`);
+})

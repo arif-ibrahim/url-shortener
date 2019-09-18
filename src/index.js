87ddad30b1d1808eeaf4bee6caba0a88faebc336
arif87 <arif.ibrahm@gmail.com>
2019-09-18 23:48:27 +0600

    add auth module

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const bp      = require('body-parser');
 const db      = require('./utils/db');
 const signup  = require('./controllers/signup')
-const login  = require('./controllers/login')
+const login   = require('./controllers/login')
+const auth    = require('./middleware/auth')
 
 app           = express();
 
 app.use(bp.json());
 app.use(signup);
 app.use(login);
+app.use('/api', auth);
 
 const _port   = process.env.PORT || 4000;
 
